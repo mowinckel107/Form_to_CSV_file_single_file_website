64eebcccc99547e8855e6fb4becaad4b33aba6b5
mowinckel107 <32161236+mowinckel107@users.noreply.github.com>
2023-08-28 11:48:09 +0200

    It works now. Single bug left
    For some reason it adds a comma as the first thing in every entry EXEPT the first one.

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 const nameInput = document.getElementById('nameInput');
 const downloadButton = document.getElementById('downloadButton');
 
-let names = [];
+
+let allInputs = [];
+
 
 downloadButton.addEventListener('click', () => {
 
-    const newName = nameInput.value.trim();
-    if (newName !== '') {
-        names.push(newName);
-        nameInput.value = '';
-    }
+    let singleInput = [];
+
+
+    const newFirstName = firstNameInput.value.trim();
+    singleInput.push(newFirstName);
+    firstNameInput.value = '';
 
 
 
-    if (names.length > 0)
-    {
-        const csvContent = "data:text/csv;charset=utf-8," + names.join('\n');
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
+    const newLastName = lastNameInput.value.trim();
+    singleInput.push(newLastName);
+    lastNameInput.value = '';
 
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "Campsite_Registrations.csv");
 
-        document.body.appendChild(link);
 
-        link.click();
-    }
+    const newCountry = countryInput.value.trim();
+    singleInput.push(newCountry);
+    countryInput.value = '';
+
+
+    const aEntry = singleInput.join(',') + '\n';
+    allInputs.push(aEntry)
+
+
+    const csvContent = "data:text/csv;charset=utf-8," + allInputs;
+    const encodedUri = encodeURI(csvContent);
+
+    const link = document.createElement("a"); // TODO try to remove. I think it is not needed
+
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "Campsite_Registrations.csv");
+
+    document.body.appendChild(link);
+
+    link.click();
+
 });
